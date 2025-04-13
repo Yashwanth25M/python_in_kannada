6848d67cf89ad850796b24ac3a0ee5c4117db6cf
Yashwanth M <yashwanthm2584@gmail.com>
2025-04-13 16:54:06 +0530

    Add files via upload

---
+++ 19vid.py
@@ -0,0 +1,44 @@
+# solving hacker rank problems
+
+# 1st question (python if-else)
+# import math
+# import os
+# import random
+# import re
+# import sys
+# if __name__ == '__main__':
+#     n = int(input().strip())
+#     if(n % 2 != 0):
+#         print("Weird")
+#     elif(n % 2 == 0 and (2 <= n >= 5)):
+#         print("Not Weird")
+#     elif(n % 2 == 0 and (6 <= n >= 20)):
+#         print("Wierd")
+#     elif(n % 2 == 0 and (20 < n)):
+#         # print("Not weird")  
+
+# import math 
+# import os
+# import random
+# import re
+# import sys
+# if __name__ == '__main__':
+#     n = int(input().strip())
+#     if(n % 2 != 0 or (6 <= n <= 20)):
+#         print("Weird")
+#     else:
+#         print("Not Weird")
+
+# # 2.Arithmetic operators
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+    
+#3 python division
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     print(a//b,"\n", a/b )
---
+++ 20vid.py
@@ -0,0 +1,34 @@
+# Solving hackerrank problems
+
+# 1.Loops
+# if __name__ == '__main__':
+#     n = int(input())
+#     for i in range (0,n) :
+#         print(i*i)
+
+# 2.write a function (full doubt)
+# def is_leap(year):
+#     leap =False
+#     if year % 4==0 :
+#         leap = True
+#     if year % 100 ==0 :
+#         return year % 400 ==0
+#     return leap 
+# year = int(input())
+
+# def is_leap(year):
+#     leap =False
+#     if year % 4 == 0:
+#         leap = True
+#     if year % 100 == 0:
+#         leap = False
+#     if year % 400 == 0:
+#         leap = True
+#     return leap 
+# year = int(input())
+
+# 3.print function
+# if __name__ == '__main__':
+#     n = int(input())
+#     for i in range(1,n+1):
+#         print(i,end="")
